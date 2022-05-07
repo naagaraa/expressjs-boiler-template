3a85e1960341fc17d4ab3fe8776197710cfba1fa
naagaraa <ekabersinar@gmail.com>
2022-05-07 22:19:20 +0700

    :update landing page

--- src/app.js
+++ src/app.js
@@ -1,21 +1,30 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 const port = 3000;
 
 // route api
 const route = require("./routes");
 
+// mount middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 // mount api
 app.use("/api/", route);
-app.use(cors());
 
 // default landing page route
-app.get("/", (req, res) => res.send("home page for api"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
 
 // start server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port http://localhost:${port}`);
 });
 
 module.exports = app;
